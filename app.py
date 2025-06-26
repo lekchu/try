a1541a5959d01a3fafad7ce0ec908330019f3ae0
lekchu <151711811+lekchu@users.noreply.github.com>
2025-06-27 00:15:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 from sklearn.ensemble import RandomForestClassifier
 
 # Train a dummy model in-app (just for testing/demo)
